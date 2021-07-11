b0c4f8d58bb4f3459526625d2f44c5fdcf128c98
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-11 23:19:53 +0100

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "patriciaXmusic"
+        user.first_name =  "KaelaVC"
 
     try:
         await USER.join_chat(invitelink)
